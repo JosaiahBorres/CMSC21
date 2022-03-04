d204cd04a5ca1878921a18fd93de3ab921f4643a
JosaiahBorres <100907809+JosaiahBorres@users.noreply.github.com>
2022-03-04 15:26:33 +0800

    Add files via upload

---
+++ Lecture1/Assignment/CMSC21_Assignment1/assignment_lec1_c(1).c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    printf("In C, lowercase letters are significant.\n");
+    printf("Main is where program execution begins.\n");
+    printf("Opening and closing braces enclose program statements in a routine.\n");
+    printf("All program statements must be terminated by a semicolon.\n");
+    
+    return 0;
+}
---
+++ Lecture1/Assignment/CMSC21_Assignment1/assignment_lec1_c2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void){
+    int number1, number2, answer;
+    number1=15;
+    number2=87;
+    answer=number2-number1;
+    printf("The answer is %d.", answer);
+    
+
+return 0;
+}
