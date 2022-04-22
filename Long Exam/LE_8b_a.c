b32ea4ae18c94122d9dfa591eef694372fa0f8c4
JosaiahBorres <100907809+JosaiahBorres@users.noreply.github.com>
2022-04-22 15:06:26 +0800

    Add files via upload

---
+++ Long Exam/LE_8b_a.c
@@ -0,0 +1,18 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a, b; //designate variables
+    a=2;
+    b=3;
+if ( (b == 3) && ( a == 2 )){ // conditionals
+printf( "*****" );
+printf( ">>>>>" );
+printf( "<<<<<" );
+}
+else{
+printf( "-----" );
+}
+    
+    return 0;
+}
